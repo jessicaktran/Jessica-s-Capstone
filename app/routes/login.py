4de95515a83e4f6be549b884beed5f2cef358da2
jessicaktran <jessicaxxtran@gmail.com>
2024-02-14 10:07:43 -0800

    changes

--- app/routes/login.py
+++ app/routes/login.py
@@ -128,20 +128,20 @@ def callback():
         thisUser=User.objects.get(email=gmail)
     # if the user does not exist, create them and make sure they are ousd.org
     except mongoengine.errors.DoesNotExist:
-        if userinfo_response.json().get("hd") == "ousd.org":
-            thisUser = User(
-                gid=gid, 
-                gname=gname, 
-                email=gmail, 
-                gprofile_pic=gprofile_pic,
-                fname = gfname,
-                lname = glname
-            )
-            thisUser.save()
-            thisUser.reload()
-        else:
-            flash("You must have an ousd.org email to login to this site.")
-            return redirect(url_for('index'))
+        # if userinfo_response.json().get("hd") == "ousd.org":
+        thisUser = User(
+            gid=gid, 
+            gname=gname, 
+            email=gmail, 
+            gprofile_pic=gprofile_pic,
+            fname = gfname,
+            lname = glname
+        )
+        thisUser.save()
+        thisUser.reload()
+        # else:
+        #     flash("You must have an ousd.org email to login to this site.")
+        #     return redirect(url_for('index'))
     else:
         thisUser.update(
             gid=gid, 
